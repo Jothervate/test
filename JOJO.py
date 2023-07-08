bf71049c80b0590fc2578883c84e659932dea84e
Jothervate <46174845+Jothervate@users.noreply.github.com>
2023-07-08 08:24:22 +0000

    modify

--- JOJO.py
+++ JOJO.py
@@ -1,18 +1,31 @@
 import random
 
-min=1
-max=10
-count=0
-target=random.randint(min,max)
+def playGame():
+    min=0
+    max=10
+    count=0
+    target=random.randint(min,max)
+
+    while(True):
+        keyin=int(input(f"猜數字範圍{min}~{max}:"))
+        count+=1
+        if(keyin==target):
+            print(f"correct,answer is {target}")
+            print(f"you use {count} times to guess")
+            break
+        elif keyin>target:
+            print("more smaller!")
+            max-=1
+        elif keyin<target:
+            print("more bigger!")
+        min+=1
+        print(f"you use {count} times to guess")
+    print("fin")
 
 while(True):
-    keyin=int(input(f"猜數字範圍{min}~{max}:"))
-    count+=1
-    if(keyin==target):
-        print(f"correct,answer is {target}")
-        print(f"you use {count} times to guess")
+    playGame()
+    Game_input=input("Do you want to play again?(y,n)")
+    if not(Game_input=='y'):
         break
-    else:
-        print("wrong")
-        print(f"you use {count} times to guess")
-print("fin")
+print("gameover")
+
