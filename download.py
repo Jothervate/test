fce34298aadf6d4db777dc03d8a170391744a958
Jothervate <46174845+Jothervate@users.noreply.github.com>
2023-07-28 15:05:06 +0000

    modify

---
+++ download.py
@@ -0,0 +1,9 @@
+import requests
+import csv
+
+url = 'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-C0032-001?Authorization=rdec-key-123-45678-011121314&format=JSON'
+
+response = requests.get(url)
+if response.status_code == 200:
+    print("下載成功")
+    weather = response.json()
