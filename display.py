e0a26a8ed1ddaec6684e9037e9ae30e61b6258b4
Johon <46174845+Jothervate@users.noreply.github.com>
2023-08-12 05:47:40 +0000

    modify

---
+++ display.py
@@ -0,0 +1,7 @@
+import requests
+url= 'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-C0032-001?Authorization=rdec-key-123-45678-011121314&format=JSON'
+
+response=requests.get(url)
+if response.status_code==200:
+    print("下載成功")
+    weather=response.json
